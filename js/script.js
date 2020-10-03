9583395a83db1ab5248986a7110e4ea679be5954
Steven Nemeth <72315703+StevenNemeth@users.noreply.github.com>
2020-10-03 16:32:47 -0400

    Fixed operators

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
+var operator;
+var tempNum;
+
 function numButton(e){
-    console.log(e.target.innerText, 'raw')
     var elem = document.getElementById('equation')
     elem.innerText+=e.target.innerText
+}
+function operate(e){
+    operator = e.target.innerText
+    var elem = document.getElementById('equation')
+    tempNum = elem.innerText
+    elem.innerText = ""
+}
+function equal(e){
+    if(operator) {
+        var elem = document.getElementById('equation')
+        var result = eval(tempNum + operator + elem.innerText)
+        elem.innerText = result
+        operator = null
+    }
 }
