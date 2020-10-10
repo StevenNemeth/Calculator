a1c9e89266102d4dbb5d02e308126fb0c6e6d9d5
Steven Nemeth <72315703+StevenNemeth@users.noreply.github.com>
2020-10-10 12:57:34 -0400

    added top display and fixed some code stuff

--- js/script.js
+++ js/script.js
@@ -1,31 +1,50 @@
 var operator;
 var tempNum;
+var elem = document.getElementById('equation')
+var expression = document.getElementById('expression')
 
 function numButton(e){
-    var elem = document.getElementById('equation')
-    elem.innerText+=e.target.innerText
+    elem.innerText += e.target.innerText
 }
 function operate(e){
+        if(elem.innerText === "" && !tempNum){
+        return
+    }
     if(operator){
-        equal()
+        if(elem.innerText == ""){
         operator = e.target.innerText
+        clearText(expression, tempNum + " " + operator)
+        return
+        }
+        var result = eval(tempNum + operator + elem.innerText)
+        tempNum = result
+        operator = e.target.innerText
+        clearText(elem)
+        clearText(expression, tempNum + " " + operator)
         return
     }
     operator = e.target.innerText
-    var elem = document.getElementById('equation')
     tempNum = elem.innerText
-    elem.innerText = ""
+    clearText(expression, tempNum + " " + operator)
+    clearText(elem)
 }
 function equal(e){
-    if(operator) {
-        var elem = document.getElementById('equation')
+    if(operator && (tempNum || tempNum === 0) && elem.innerText) {
         var result = eval(tempNum + operator + elem.innerText)
         elem.innerText = result
+        clearText(expression)
         operator = null
     }
 }
-function clearButton(e){
-    var elem = document.getElementById('equation')
-    elem.innerText = ""
+function clearButton(e){  
+    clearText(elem)
+    clearText(expression)
     operator = null
+}
+function clearText(r, text){
+    if (!text){
+        r.innerText = ""
+        return
+    } 
+    r.innerText = text
 }
